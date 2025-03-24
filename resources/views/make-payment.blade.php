a06897f7a3f11162350ded192bd496465aa5b9e5
Umar Habib <mumarhabibrb102@gmail.com>
2025-03-24 16:38:20 +0500

    alert added

--- resources/views/make-payment.blade.php
+++ resources/views/make-payment.blade.php
@@ -4,18 +4,21 @@
         <section class="make-a-payment-container">
             <h1>Make a Payment</h1>
             <form class="make-a-payment-form" action="">
-                <input type="hidden" name="verification_code" id="verification_code" @if (isset($code)) value="{{ $code }}" @endif>
+                <input type="hidden" name="verification_code" id="verification_code"
+                    @if (isset($code)) value="{{ $code }}" @endif>
                 <div class="make-a-payment-column">
                     <div class="make-a-payment-form-main">
                         <div class="make-a-payment-form-left">
                             <div class="form-group">
                                 <label for="name-input">Name</label>
-                                <input type="text" id="name-input" name="name" @if (isset($name) ) value="{{ $name }}" @endif required />
+                                <input type="text" id="name-input" name="name"
+                                    @if (isset($name)) value="{{ $name }}" @endif required />
                             </div>
 
                             <div class="form-group">
                                 <label for="email-input">Email</label>
-                                <input type="email" id="email-input" class="form-control" name="email" @if (isset($email) ) value="{{ $email }}" @endif required />
+                                <input type="email" id="email-input" class="form-control" name="email"
+                                    @if (isset($email)) value="{{ $email }}" @endif required />
                             </div>
 
                             <div class="form-group">
@@ -25,10 +28,14 @@
                             </div>
                         </div>
                         <div class="make-a-payment-form-right">
-                            <a href="#" id="send-code"><img src="{{ asset('assets/images/confirm.png')}}" /></a>
+                            <a href="#" id="send-code"><img src="{{ asset('assets/images/confirm.png') }}" /></a>
                         </div>
                     </div>
-                    <a href="#" style="margin-top: 10px" class="btn" id="confirm-code">CONFIRM CODE</a>
+                    @if (isset($code))
+                        <a href="#" style="margin-top: 10px" class="btn" id="confirm-code-first">CONFIRM CODE</a>
+                    @else
+                        <a href="#" style="margin-top: 10px" class="btn" id="confirm-code">CONFIRM CODE</a>
+                    @endif
 
                     <p class="make-a-payment-text">
                         Remember to check the form and verify that the item you are paying
@@ -47,13 +54,13 @@
 
                     <div class="make-a-payments-images">
                         <div class="make-a-payments-images-rows">
-                            <img src="{{ asset('assets/images/apple-pay.png')}}" alt="apple-pay" />
-                            <img src="{{ asset('assets/images/ame.png')}}" alt="apple-pay" />
-                            <img src="{{ asset('assets/images/google-pay.png')}}" alt="google-pay" />
+                            <img src="{{ asset('assets/images/apple-pay.png') }}" alt="apple-pay" />
+                            <img src="{{ asset('assets/images/ame.png') }}" alt="apple-pay" />
+                            <img src="{{ asset('assets/images/google-pay.png') }}" alt="google-pay" />
                         </div>
                         <div class="make-a-payments-images-rows">
-                            <img src="{{ asset('assets/images/paypal.png')}}" alt="paypal" />
-                            <img src="{{ asset('assets/images/Master.png')}}" />
+                            <img src="{{ asset('assets/images/paypal.png') }}" alt="paypal" />
+                            <img src="{{ asset('assets/images/Master.png') }}" />
                         </div>
                     </div>
                 </div>
@@ -71,20 +78,20 @@
                     </div>
 
                     <p class="make-a-payment-seller-price">Seller Price</p>
-                    <p class="make-a-payment-price" id="seller_price_field">00 €</p>
+                    <p class="make-a-payment-price" id="seller_price_field">0 €</p>
                     <div class="make-a-payment-price-details">
                         <p class="make-a-payment-fee-details">
                             The fees amount
-                            <span class="make-a-payment-fee-details-span" id="fee_field">00 €</span>
+                            <span class="make-a-payment-fee-details-span" id="fee_field">0 €</span>
                         </p>
                     </div>
                     <p class="make-a-payment-seller-price">You will pay</p>
-                    <p class="make-a-payment-price make-a-payment-price01" id="you_pay_field">00 €</p>
+                    <p class="make-a-payment-price make-a-payment-price01" id="you_pay_field">0 €</p>
                     <div class="buy-follow-receive__buttons">
                         <div class="btn-box1">
                             <button class="buy-follow-receive__btn">
                                 <p>PROCEED TO PAY</p>
-                                <img src="{{ asset('assets/images/fav.png')}}" alt="" />
+                                <img src="{{ asset('assets/images/fav.png') }}" alt="" />
                             </button>
                         </div>
                     </div>
@@ -95,10 +102,14 @@
 @endsection
 @section('script')
     <script>
-        $(document).ready(function(){
+        $(document).ready(function() {
 
-             // ------------ email verification ------------
-             $('body').on('click', '#send-code', function(e) {
+            $('body').on('click','#confirm-code-first',function(e){
+                e.preventDefault();
+                toastr.error('Please verify your name and email first');
+            });
+            // ------------ email verification ------------
+            $('body').on('click', '#send-code', function(e) {
                 e.preventDefault();
                 let email = $('#email-input').val();
                 let name = $('#name-input').val();
@@ -109,8 +120,8 @@
                 }
                 let data = {
                     email: email,
-                    name : name,
-                    source : 'make_payment',
+                    name: name,
+                    source: 'make_payment',
                     _token: csrfToken
                 };
                 $.ajax({
@@ -131,8 +142,8 @@
                 });
             });
 
-             // -------------- get transaction data --------------
-             $('body').on('click', '#confirm-code', function(e) {
+            // -------------- get transaction data --------------
+            $('body').on('click', '#confirm-code', function(e) {
                 e.preventDefault();
                 let seller_code = $('#seller-code-input').val();
                 if (!seller_code) {
@@ -152,9 +163,13 @@
                             if (response.error) {
                                 toastr.error(response.message);
                             } else {
-                                $('#seller_price_field').html(response.transaction.price + response.transaction.currency_symbol);
-                                $('#fee_field').html(response.transaction.fee_price + response.transaction.currency_symbol);
-                                $('#you_pay_field').html(parseInt(response.transaction.fee_price) + parseInt(response.transaction.price) +  response.transaction.currency_symbol);
+                                $('#seller_price_field').html(response.transaction.price +
+                                    response.transaction.currency_symbol);
+                                $('#fee_field').html(response.transaction.fee_price + response
+                                    .transaction.currency_symbol);
+                                $('#you_pay_field').html(parseInt(response.transaction
+                                        .fee_price) + parseInt(response.transaction.price) +
+                                    response.transaction.currency_symbol);
                                 $('#The-payment-is-for').val(response.transaction.title);
                                 $('#In-two-words').html(response.transaction.words);
                                 toastr.success(response.message);
