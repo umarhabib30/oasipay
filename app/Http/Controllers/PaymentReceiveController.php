6d20970a2174002dc48b4404bffaa57c41a41a99
Umar Habib <mumarhabibrb102@gmail.com>
2025-04-24 22:56:20 +0500

    item received logic

--- app/Http/Controllers/PaymentReceiveController.php
+++ app/Http/Controllers/PaymentReceiveController.php
@@ -42,6 +42,13 @@ class PaymentReceiveController extends Controller
                     return redirect('/')->with('error', $message);
                 }
 
+                if($transaction->item_received == 1){
+                    return response()->json([
+                        'error' => true,
+                        'message' => 'Item already received',
+                    ]);
+                }
+
                 return response()->json([
                     'error' => false,
                     'message' => 'Transaction found successfully',
