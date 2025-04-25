2dceb1e78374dde95296ffe15e9df6f719213363
Umar Habib <mumarhabibrb102@gmail.com>
2025-04-25 22:05:36 +0500

    card page updated

--- resources/views/payment/start.blade.php
+++ resources/views/payment/start.blade.php
@@ -1,6 +1,12 @@
-@extends('layouts.app')
-
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
     <h2>Pay with Card</h2>
 
     @if ($errors->any())
@@ -28,4 +34,6 @@
 
         <button type="submit">Pay Now</button>
     </form>
-@endsection
+</body>
+</html>
+
