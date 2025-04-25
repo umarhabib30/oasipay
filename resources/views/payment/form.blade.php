0237b98bae8c8c89b26f385fb482c1045bb20dc7
Umar Habib <mumarhabibrb102@gmail.com>
2025-04-26 01:16:07 +0500

    redirect url

--- resources/views/payment/form.blade.php
+++ resources/views/payment/form.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
-@section('content')
-<div class="iframe_div" style="width: 100%; height: 100vh; overflow: hidden; position: relative;">
-    <iframe src="{{ $url }}" frameborder="0" width="100%" height="100%"></iframe>
-
-</div>
-
+@section('script')
+<script>
+    window.location.href = "{{ $url }}";
+</script>
 @endsection
