d840d7c25e026306fba115799d7b64407b0775ec
Umar Habib <mumarhabibrb102@gmail.com>
2025-04-26 01:02:15 +0500

    height 100%

--- resources/views/payment/form.blade.php
+++ resources/views/payment/form.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-<iframe src="{{ $url }}" frameborder="0" width="100%" height="50%"></iframe>
+<iframe src="{{ $url }}" frameborder="0" width="100%" height="100%"></iframe>
 @endsection
