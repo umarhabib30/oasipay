17e0f11eebbe68352974f494c56a3267d84ede66
Umar Habib <mumarhabibrb102@gmail.com>
2025-04-26 01:09:09 +0500

    comment

--- resources/views/payment/form.blade.php
+++ resources/views/payment/form.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<iframe src="{{ $url }}" frameborder="0" width="100%" height="100%"></iframe>
+<div class="iframe_div" style="width: 100%; height: 100vh; overflow: hidden; position: relative;">
+    <iframe src="{{ $url }}" frameborder="0" width="100%" height="100%"></iframe>
+
+</div>
+
 @endsection
